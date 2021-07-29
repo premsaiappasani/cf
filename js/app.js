f37632a5a31f83251a73935d5a82c041312d064c
Prem Sai <premsaiappasani@gmail.com>
2021-07-29 15:37:52 +0530

    up1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let handle=prompt("Enter you codeforces handle");
+function start(){
+    fn1();
+}
+function fn1(){
+    fetch(`https://codeforces.com/api/user.status?handle=${handle}&from=1&count=1`)
+        .then(res=>{
+            console.log(res,res.json())
+            return res.json();
+            })
+        .then(data=>{
+            console.log(data);
+            if(data.status==="OK"){
+                console.log(data.result[0].verdict);
+                if(data.result[0].verdict=="TESTING"){
+                    document.getElementById("div").innerText+=".";
+                    setTimeout(100,fn1());
+                }
+                else{
+                    console.log(data.result[0].verdict);
+                    alert(data.result[0].verdict);
+                }
+            }
+        })
+        .catch(e=>{
+            console.log("err",e);
+        })
+}
