e6204e0b17c92b8bd241b8026020c82ce60424ef
Prem Sai <premsaiappasani@gmail.com>
2021-07-30 19:53:15 +0530

    added audio effects

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 let handle=prompt("Enter you codeforces handle");
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+}
+var winsound= new sound("audio/win.wav");
+var losesound= new sound("audio/lose.wav");
 function fn1(){
     fetch(`https://codeforces.com/api/user.status?handle=${handle}&from=1&count=1`)
         .then(res=>{
@@ -12,8 +29,12 @@ function fn1(){
                     document.getElementById("div").innerText+=".";
                     setTimeout(100,fn1());
                 }
+                else if(data.result[0].verdict=="OK"){
+                    winsound.play();
+                    alert(`Last Submission: ${data.result[0].verdict}`);
+                }
                 else{
-                    console.log(data.result[0].verdict);
+                    losesound.play();
                     alert(`Last Submission: ${data.result[0].verdict}`);
                 }
             }
